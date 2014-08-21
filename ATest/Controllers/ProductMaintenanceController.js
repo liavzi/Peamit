efe927868bc406cf9d365f69994ee632e6df8a26
liavzi <liavzi@gmail.com>
2014-08-21 18:10:07 +0300

    x

---
+++ ATest/Controllers/ProductMaintenanceController.js
@@ -0,0 +1,5 @@
+﻿((function() {
+    var app = angular.module("AngularTest");
+    app.controller('ProductMaintenanceController', function () {
+    });
+})())
---
+++ ATest/Scripts/app.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    var app = angular.module('AngularTest', ['ui.bootstrap','ngRoute']);
+    app.config(['$routeProvider','$locationProvider',function ($routeProvider,$locationProvider) {
+        $routeProvider.
+            when('/', {
+                templateUrl: 'Views/MainPage.html',
+            }).
+            when('/ProductMaintenance', {
+                templateUrl: 'Views/ProductMaintenance.html',
+            }).
+            otherwise({
+                redirectTo: '/'
+            });
+    }]);
+} ())
