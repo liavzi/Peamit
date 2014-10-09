a3262e74545560d272655b7fc2739487b0b6b233
liavzi <liavzi@gmail.com>
2014-10-09 23:09:32 +0300

    finished product service

--- Node/src/routers/productRouter.js
+++ Node/src/routers/productRouter.js
@@ -8,6 +8,27 @@ productRouter.route("/products")
 			res.json(product);
 			res.end();
 		});		
+	})
+	.get(function(req,res){
+		productService.getAllProducts(function(err,products){
+			if (err) res.end(err.toString());
+			res.json(products);
+			res.end();
+		});
+	});
+productRouter.route("/products/:productId")
+	.get(function (req,res){
+		productService.getProduct(req.params.productId,function(err,product){
+			if (err) res.end(err.toString());
+			res.json(product);
+			res.end();
+		});		
+	})
+	.put(function(req,res){
+		productService.updateProduct(req.body,function(err){
+			if (err) res.end(err.toString());
+			res.end('{"success" : "Updated Successfully", "status" : 200}');
+		});
 	});
 
 module.exports = productRouter;
--- Node/src/services/productService.js
+++ Node/src/services/productService.js
@@ -3,12 +3,29 @@ var Product = require("../models/ProductModel");
 
 var productService = {
 	createProduct : function(product,callback){
-		console.log(product);
 		Product.create(product,function(err,savedProduct){
 			if (err) callback(err);
 			callback(err,savedProduct);
 		});
-	}
+	},
+	getAllProducts : function(callback){
+		Product.find({},function(err,products){
+			if (err) callback(err);
+			callback(err,products);
+		});
+	},
+	getProduct : function(productId,callback){
+		Product.findById(productId,function(err,product){
+			if (err) callback(err);
+			callback(err,product);
+		});
+	},
+	updateProduct : function(product,callback){
+		Product.update({_id:product._id},product,function(err){
+			if (err) callback(err);
+			callback();
+		});
+	},
 };
 
 module.exports = productService;
