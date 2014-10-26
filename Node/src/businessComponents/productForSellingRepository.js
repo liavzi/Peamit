fd065d9cb712f4db0cc9ebcac269766ce2b7c75c
liavzi <liavzi@gmail.com>
2014-10-26 09:47:56 +0200

    ref

--- Node/src/businessComponents/productForSellingRepository.js
+++ Node/src/businessComponents/productForSellingRepository.js
@@ -5,22 +5,32 @@ var async = require("async");
 var productForSellingRepository = {
     getById : function(productId,callback){
         async.parallel([
-            function(callback){
-                Product.findById(productId,callback);
-            },
-            function(callback){
-                Price.find().where("productId").equals(productId).exec(callback)
-            }
-        ],function(err,results){
-            if (err) callback(err);
-            var product = {};
-            product._id = results[0]._id;
-            product.name = results[0].name;
-            var price = results[1][0];
-            product.price = price.value;
-            callback(null,product);
-        });
+            function(callback){getProduct(productId,callback)},
+            function(callback){getPrice(productId,callback)}],
+            createAndReturnProductForSelling(callback));
     }
+};
+
+var getProduct =  function (productId, callback) {
+    Product.findById(productId,callback);
+};
+
+var getPrice =  function (productId, callback) {
+    Price.find().where("productId").equals(productId).exec(callback)
+};
+
+function createProductForSelling(results) {
+    var product = results[0].toObject();
+    var price = results[1][0];
+    product.price = price.value;
+    return product;
+}
+var createAndReturnProductForSelling = function (callback){
+    return function(err,results) {
+        if (err) callback(err);
+        var product = createProductForSelling(results);
+        callback(null, product);
+    };
 };
 
 module.exports = productForSellingRepository;
