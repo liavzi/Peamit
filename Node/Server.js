92f9573fc13b51ca4f68613f174955336d362607
liavzi <liavzi@gmail.com>
2015-01-05 10:33:34 +0200

    public files

--- Node/Server.js
+++ Node/Server.js
@@ -15,27 +15,9 @@ mongoose.connect(databaseConfig.url);
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods","GET, POST, PUT,DELETE");
-    next();
-});
+app.use(express.static(__dirname + '/public'));
 
 var port = process.env.PORT || 8080; 		// set our port
-
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router(); 				// get an instance of the express Router
-
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res,next) {
-	console.log("incoming request...");
-	next();
-});
-
-// more routes for our API will happen here
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
