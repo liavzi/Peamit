f8036a3ab29e3a282ccc15f3e23f40516ec90af1
liavzi <liavzi@gmail.com>
2015-03-10 17:34:53 +0200

    refactoring

--- Node/src/routers/registerRouters.js
+++ Node/src/routers/registerRouters.js
@@ -1,19 +1,13 @@
 var productForSellingRouter = require("../routers/productForSellingRouter");
 var genericRouter = require("./genericRouter");
-var addItemToOrderRouter = require("./addItemToOrderRouter");
-var orderLinesRouter = require("./orderLinesRouter");
-var closeOrderByPhoneRouter = require("./closeOrderByPhoneRouter");
+var orderRouter = require("./orderRouter");
 var Order = require("../models/OrderModel");
 function registerRouters (express,app){
     var apiRouter = express.Router();
-    var orderRouter = express.Router();
     apiRouter.use(productForSellingRouter);
     apiRouter.use(genericRouter);
-    orderRouter.use(addItemToOrderRouter);
-    orderRouter.use(orderLinesRouter);
-    orderRouter.use(closeOrderByPhoneRouter);
-    apiRouter.all("/orders/:orderId/*",loadOrder);
-    apiRouter.use("/orders/:orderId",orderRouter)
+    apiRouter.use("/orders/:orderId",loadOrder);
+    apiRouter.use("/orders/:orderId",orderRouter);
     app.use("/api",apiRouter);
 }
 
