5442867f0d203bbb66e53eed842625876c3c244b
liavzi <liavzi@gmail.com>
2015-03-24 17:56:43 +0200

    refactoring

--- Node/gulpfile.js
+++ Node/gulpfile.js
@@ -1,6 +1,10 @@
-var gulp = require('gulp');
+var gulp = require("gulp");
+var plugin = require("gulp-load-plugins")();
 
-gulp.task('default', function() {
+gulp.task("default", function() {
 
+    return gulp.src(["src/**/*.js","!src/client/ScriptsLibs/**.js"])
+        .pipe(plugin.jshint())
+        .pipe(plugin.jshint.reporter("jshint-stylish"))
 });
 
